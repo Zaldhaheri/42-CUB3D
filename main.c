18af6ad2ac48e0cdbaee5bec9bd7eacc1bd64374
zaldhaheri <zayed.ahmed123321@gmail.com>
2025-02-02 21:11:27 +0400

    added structs, safety on pixel_put

--- main.c
+++ main.c
@@ -4,6 +4,11 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
 	char	*dst;
 
+	if (x < 0 || y < 0 || x >= S_W || y >= S_H)
+	{
+		printf("pixel out of range!\n");
+		return ;
+	}
 	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
 	*(unsigned int*)dst = color;
 }
@@ -15,40 +20,6 @@ void init(t_data *data)
 	data->img = mlx_new_image(data->mlx, S_W, S_H);
 	data->addr = mlx_get_data_addr(data->img, &data->bits_per_pixel, &data->line_length, &data->endian);
 }
-
-// void draw_box(t_data *data, int x_start, int y_start, int width, int height, int color)
-// {
-//     for (int y = y_start; y < y_start + height; y++)
-//     {
-//         for (int x = x_start; x < x_start + width; x++)
-//         {
-//             my_mlx_pixel_put(data, x, y, color);
-//         }
-//     }
-// }
-
-// void draw_circle(t_data *data, int xc, int yc, int radius, int color)
-// {
-//     for (int y = yc - radius; y <= yc + radius; y++)
-//     {
-//         for (int x = xc - radius; x <= xc + radius; x++)
-//         {
-//             if ((x - xc) * (x - xc) + (y - yc) * (y - yc) <= radius * radius) // Circle equation (x - xc)² + (y - yc)² ≤ r²
-//             {
-//                 my_mlx_pixel_put(data, x, y, color);
-//             }
-//         }
-//     }
-// }
-
-// void draw_triangle(t_data *data, int x, int y, int h, int color)
-// {
-//     for (int y1 = y; y1 < h + y; y1++)
-//     {
-//         for (int x1 = x; x1 < x + (y1 - y); x1++)
-//             my_mlx_pixel_put(data, x1, y1, color);
-//     }
-// }
 
 void set_FC(t_data *data)
 {
--- raycast.h
+++ raycast.h
@@ -13,26 +13,43 @@
 # define C_FLOOR 0x1A9D0D
 # define C_CEILING 0x335DFF
 
+// Variables with player
 typedef struct s_player
 {
-
+	int plr_pxl_x;
+	int plr_pxl_y;
+	
 } t_player;
 
-typedef struct s_raycast
+// Variables with rays
+typedef struct s_ray
 {
+	double ray_distance;
+	double ray_angle;
+	int flag;
+} t_ray;
 
-} t_raycast;
+// Variables with maps (max)
+typedef struct s_map
+{
+	char **map_arr;
+	int map_w;
+	int map_h;
+} t_map;
 
+// Main struct
 typedef struct s_data
 {
-	void *mlx;
-	void *win;
-	void *img;
-	char *addr;
-	int bits_per_pixel;
-	int line_length;
-	int endian;
-	
+	void		*mlx;
+	void		*win;
+	void		*img;
+	char		*addr;
+	int			bits_per_pixel;
+	int			line_length;
+	int			endian;
+	t_player	*plr;
+	t_ray		*ray;
+	t_map		*map;
 } t_data;
 
 #endif
