36bb051dccb510e8761aaa5e7c7f72baa70ad071
Zayed Aldhaheri <zaldhahe@lab1r1s12.42abudhabi.ae>
2025-05-19 14:09:14 +0400

    collision is smoother, added textures

--- raycast2.c
+++ raycast2.c
@@ -18,6 +18,12 @@ void paint(t_data *data, t_ray *ray, t_player *player, t_line *line)
 {
 	(void)player;
 	int y;
+	unsigned int (*tex)[64];
+	double wall_x;
+	int tex_x;
+	int d;
+	int tex_y;
+	unsigned int color;
 
 	y = 0;
 	line->x = ray->pxl_x;
@@ -26,19 +32,30 @@ void paint(t_data *data, t_ray *ray, t_player *player, t_line *line)
 		my_mlx_pixel_put(data, line->x, y, C_CEILING);
 		y++;
 	}
-	y = ray->draw_s;
-	int wall_color = 0xA45670;
 	if (ray->side == NORTH)
-		wall_color = 0xFF0000; // red
+		tex = data->textures.north;	
 	else if (ray->side == SOUTH)
-		wall_color = 0x00FF00; // green
+		tex = data->textures.south;
 	else if (ray->side == EAST)
-		wall_color = 0x0000FF; // blue
+		tex = data->textures.east;
 	else if (ray->side == WEST)
-		wall_color = 0xFFFF00; // yellow
+		tex = data->textures.west;
+
+	if (ray->side == EAST || ray->side == WEST)
+		wall_x = data->plr->pos_y + ray->wall_dst * ray->dir_y;
+	else
+		wall_x = data->plr->pos_x + ray->wall_dst * ray->dir_x;
+	wall_x -= (int) wall_x;
+	tex_x = (int) (wall_x * 64);
+	if (ray->side == EAST || ray->side == SOUTH)
+		tex_x = 64 - tex_x - 1;
+	y = ray->draw_s;
 	while (y < ray->draw_e)
 	{
-		my_mlx_pixel_put(data, line->x, y, wall_color);
+		d = y * 256 - S_H * 128 + ray->line_height * 128;
+		tex_y = ((d * 64) / ray->line_height) / 256;
+		color = tex[tex_y][tex_x];
+		my_mlx_pixel_put(data, line->x, y, color);
 		y++;
 	}
 	y = ray->draw_e;
